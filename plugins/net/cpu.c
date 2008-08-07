aa0fc47506be4c97c5350b3decdd1c740e45030f
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2008-08-07 19:03:41 +0000

    net monitor initial submit

---
+++ plugins/net/cpu.c
@@ -0,0 +1,122 @@
+/*
+ * CPU usage plugin to fbpanel
+ *
+ * Copyright (C) 2004 by Alexandre Pereira da Silva <alexandre.pereira@poli.usp.br>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+/*A little bug fixed by Mykola <mykola@2ka.mipt.ru>:) */
+
+
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/sysinfo.h>
+#include <stdlib.h>
+
+#include "misc.h"
+#include "../chart/chart.h"
+
+//#define DEBUG
+#include "dbg.h"
+
+
+
+/* cpu.c */
+struct cpu_stat {
+    gulong u, n, s, i;
+};
+
+typedef struct {
+    chart_t chart;
+    struct cpu_stat cpu_prev;
+    int timer;
+} cpu_t;
+
+static chart_class_t *k;
+
+static int
+cpu_get_load(cpu_t *c)
+{
+    gfloat a, b;
+    struct cpu_stat cpu, cpu_diff;
+    FILE *stat;
+    float total;
+
+    ENTER;
+    stat = fopen("/proc/stat", "r");
+    if(!stat)
+        RET(TRUE);
+    fscanf(stat, "cpu %lu %lu %lu %lu", &cpu.u, &cpu.n, &cpu.s, &cpu.i);
+    fclose(stat);
+
+    cpu_diff.u = cpu.u - c->cpu_prev.u;
+    cpu_diff.n = cpu.n - c->cpu_prev.n;
+    cpu_diff.s = cpu.s - c->cpu_prev.s;
+    cpu_diff.i = cpu.i - c->cpu_prev.i;
+    c->cpu_prev = cpu;
+
+    a = cpu_diff.u + cpu_diff.n + cpu_diff.s;
+    b = a + cpu_diff.i;
+    total = b ?  a / b : 1.0;
+    DBG("total=%f a=%f b=%f\n", total, a, b);
+    k->add_tick(&c->chart, total);
+    RET(TRUE);
+
+}
+
+
+static int
+cpu_constructor(plugin *p)
+{
+    cpu_t *c;
+
+    if (!(k = class_get("chart")))
+        RET(0);
+    c = p->priv = g_new0(cpu_t, 1);
+    if (!k->constructor(p))
+        RET(0);
+    c->timer = g_timeout_add(1000, (GSourceFunc) cpu_get_load, (gpointer) c);
+    RET(1);
+}
+
+
+static void
+cpu_destructor(plugin *p)
+{
+    cpu_t *c = (cpu_t *) p->priv;
+
+    ENTER;
+    g_source_remove(c->timer);
+    k->destructor(p);
+    g_free(p->priv);
+    class_put("chart");
+    RET();
+}
+
+
+plugin_class cpu_plugin_class = {
+    fname: NULL,
+    count: 0,
+
+    type : "cpu",
+    name : "Cpu usage",
+    version: "1.0",
+    description : "Display cpu usage",
+
+    constructor : cpu_constructor,
+    destructor  : cpu_destructor,
+};

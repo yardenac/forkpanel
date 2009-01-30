40103935c054226445887c56ba0662e721a40ebd
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2009-01-30 16:42:34 +0000

    makefile restructuring

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -5,25 +5,29 @@
 #########################
 
 # Note 1: PWD will be that of configure, not scripts directory, so to run script from
-#         this directory refer it as 'scripts/name'
+# this directory refer it as 'scripts/name'
+
 # Note 2: values will be evaluated in the same order they were added, so
-#         if you want libdir's default value to be '$eprefix/lib', add it
-#         after prefix
+# if you want libdir's default value to be '$eprefix/lib', add it after prefix
 
-# variable reference
-#add_var user "test variable" joe
-#add_var home "test variable" '/home/$user'
-
-#add_var rfs "rfs" /tmp/rfs
-#add_var glib_cflags "glib cflags" '`RFS=$rfs scripts/pc.sh --cflags glib-2.0`'
-#add_var glib_libs "glib libs" '`RFS=$rfs scripts/pc.sh --libs glib-2.0`'
-
-# autodetection: default value as output of command
-add_feature dependency "disable dependency tracking" disabled
-add_var gtk_cflags "gtk cflags" '`scripts/pc.sh --cflags gtk+-2.0`'
-add_var gtk_libs "gtk libs" '`scripts/pc.sh --libs gtk+-2.0`'
-add_var gmodule_libs "gtk libs" '`scripts/pc.sh --libs gmodule-2.0`'
-#add_var endianess "detect endianess (big or little)" '`scripts/endianess.sh`'
+# Usefull stuff
+add_var endianess "detect endianess (big or little)" '`scripts/endianess.sh`'
 add_var os "detect OS flavour" '`uname -s | tr [:lower:] [:upper:]`'
-add_var version "package version" '`cat version`'
 
+# Root File System for embedded or cross-compiler builds
+#add_var rfs "embedded RFS (root file system)" /nfsboot/rfs
+
+
+# Custom
+add_feature dependency "disable dependency tracking" disabled
+
+add_var glib_cflags "glib cflags" '`RFS=$rfs scripts/rfs-pkg-config --cflags glib-2.0`'
+add_var gtk_cflags "gtk cflags" '`RFS=$rfs scripts/rfs-pkg-config --cflags gtk+-2.0`'
+
+add_var glib_libs "glib libs" '`RFS=$rfs scripts/rfs-pkg-config --libs glib-2.0`'
+add_var gtk_libs "gtk libs" '`RFS=$rfs scripts/rfs-pkg-config --libs gtk+-2.0`'
+
+add_var cflags "C flags" '$glib_cflags $gtk_cflags -fPIC'
+add_var ldflags "linker flags" '$glib_libs $gtk_libs'
+
+add_var gmodule_libs "gmodule libs" '`scripts/rfs-pkg-config --libs gmodule-2.0`'

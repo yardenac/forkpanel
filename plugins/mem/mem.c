bbd9b9b17f94f81a3b0db89c1bdf0d01a46ae5cd
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2010-01-24 05:43:34 +0000

    Mem usage plugin

--- plugins/mem/mem.c
+++ plugins/mem/mem.c
@@ -13,84 +13,205 @@
 //#define DEBUGPRN
 #include "dbg.h"
 
-extern panel *the_panel;
 
-
-typedef struct {
-    plugin_instance *plugin;
-    GtkWidget *pb;
+typedef struct
+{
+    plugin_instance plugin;
+    GtkWidget *mem_pb;
+    GtkWidget *swap_pb;
+    GtkWidget *box;
     int timer;
+    int show_swap;
 } mem_priv;
 
-//static mem me;
-
-
-static gint
-clock_update(mem_priv *dc)
+typedef struct
 {
-    static gdouble p;
-          
-    ENTER;
-    p += 0.1;
-    if (p > 1)
-        p = 0.0;
-    gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR(dc->pb), p);
-    RET(TRUE);
+    char *name;
+    gdouble val;
+    int valid;
+} mem_type_t;
+
+
+#if defined __linux__
+#undef MT_ADD
+#define MT_ADD(x) MT_ ## x,
+enum {
+#include "mt.h"
+    MT_NUM
+};
+
+#undef MT_ADD
+#define MT_ADD(x) { #x, 0, 0 },
+mem_type_t mt[] =
+{
+#include "mt.h"
+};
+
+static gboolean
+mt_match(char *buf, mem_type_t *m)
+{
+    gulong val;
+    int len;
+
+    len = strlen(m->name);
+    if (strncmp(buf, m->name, len))
+        return FALSE;
+    if (sscanf(buf + len + 1, "%lu", &val) != 1)
+        return FALSE;
+    m->val = (gdouble) val;
+    m->valid = 1;
+    DBG("%s: %lu\n", m->name, val);
+    return TRUE;
 }
 
-
-static int
-mem_constructor(plugin_instance *p)
+static void
+mem_usage(gdouble *mu, gdouble *su)
 {
-    mem_priv *dc;
-    GdkColor color;
-    GtkRcStyle *rc;
-    
+    FILE *fp;
+    char buf[160];
+    int i;
+
+    *mu = *su = 0;
+    fp = fopen("/proc/meminfo", "r");
+    if (!fp)
+        return;
+    for (i = 0; i < MT_NUM; i++)
+    {
+        mt[i].valid = 0;
+        mt[i].val = 0;
+    }
+
+    while ((fgets(buf, sizeof(buf), fp)) != NULL)
+    {
+        for (i = 0; i < MT_NUM; i++)
+        {
+            if (!mt[i].valid && mt_match(buf, mt + i))
+                break;
+        }
+    }
+    fclose(fp);
+  
+    if (mt[MT_MemTotal].val == 0.0)
+    {
+        DBG("%s is zero\n", mt[MT_MemTotal].name);
+        return;
+    }
+    *mu = 1 - ((mt[MT_MemFree].val + mt[MT_Buffers].val + mt[MT_Cached].val +
+                    mt[MT_Slab].val) / mt[MT_MemTotal].val);
+    *su = 1 - (mt[MT_SwapFree].val / mt[MT_SwapTotal].val);
+}
+#else
+static void
+mem_usage(gdouble *mu, gdouble *su)
+{
+    *mu = *su = 0;
+}
+#endif
+
+static void
+mem_update(mem_priv *mem)
+{
+    gdouble mu, su;
+    char str[40];
     ENTER;
-    dc = g_new0(mem_priv, 1);
-    g_return_val_if_fail(dc != NULL, 0);
-    p->priv = dc;
-    
-    dc->pb = gtk_progress_bar_new();
-    rc = gtk_widget_get_modifier_style(dc->pb);
-    if (!rc) {
-        rc = gtk_rc_style_new();
-    }
-    if (rc) {
-        rc->color_flags[GTK_STATE_PRELIGHT] |= GTK_RC_BG;
-        gdk_color_parse("green", &color);
-        rc->bg[GTK_STATE_PRELIGHT] = color;
-    }
-    gtk_widget_modify_style(dc->pb, rc);
-    if (the_panel->orientation == ORIENT_HORIZ) {
-        gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR(dc->pb), GTK_PROGRESS_BOTTOM_TO_TOP);
-        gtk_widget_set_size_request(dc->pb, 9, 0);
-    } else {
-        gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR(dc->pb), GTK_PROGRESS_LEFT_TO_RIGHT);
-        gtk_widget_set_size_request(dc->pb, 0, 9);
-    }
-    gtk_widget_show_all(dc->pb);
-    dc->timer = g_timeout_add(1000, (GSourceFunc) clock_update, (gpointer)dc);
-    gtk_container_add(GTK_CONTAINER(p->pwid), dc->pb);
-    RET(1);
+    mem_usage(&mu, &su);
+    g_snprintf(str, sizeof(str), "<b>Mem: %d%%</b>\n<b>Swap: %d%%</b>",
+            (int)(mu * 100), (int) (su * 100));
+    gtk_widget_set_tooltip_markup(mem->plugin.pwid, str);
+    gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR(mem->mem_pb), mu);
+    if (mem->show_swap)
+        gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR(mem->swap_pb), su);
 }
 
 
 static void
 mem_destructor(plugin_instance *p)
 {
-    mem_priv *dc = (mem_priv *)p->priv;
+    mem_priv *mem = (mem_priv *)p;
 
     ENTER;
-    dc = (mem_priv *) p->priv;
-    if (dc->timer)
-        g_source_remove(dc->timer);
-    gtk_widget_destroy(dc->pb);
-    g_free(dc);
+    if (mem->timer)
+        g_source_remove(mem->timer);
+    gtk_widget_destroy(mem->box);
     RET();
 }
 
+static int
+mem_constructor(plugin_instance *p)
+{
+    mem_priv *mem;
+    gint w, h;
+    GtkProgressBarOrientation o;
+    line s;
+    
+    ENTER;
+    mem = (mem_priv *) p;
+    while (get_line(p->fp, &s) != LINE_BLOCK_END)
+    {
+        if (s.type == LINE_NONE)
+        {
+            ERR( "space: illegal token %s\n", s.str);
+            goto error;
+        }
+        if (s.type == LINE_VAR)
+        {
+            if (!g_ascii_strcasecmp(s.t[0], "ShowSwap"))
+                mem->show_swap = str2num(bool_pair, s.t[1], 1);
+            else
+            {
+                ERR("mem: unknown var %s\n", s.t[0]);
+                goto error;
+            }
+        }
+        else
+        {
+            ERR("menu: illegal in this context %s\n", s.str);
+            goto error;
+        }
+    }
+    mem->box = p->panel->my_box_new(FALSE, 0);
+    gtk_container_set_border_width (GTK_CONTAINER (mem->box), 0);
+
+    if (p->panel->orientation == ORIENT_HORIZ)
+    {
+        o = GTK_PROGRESS_BOTTOM_TO_TOP;
+        w = 9;
+        h = 0;
+    }
+    else
+    {
+        o = GTK_PROGRESS_LEFT_TO_RIGHT;
+        w = 0;
+        h = 9;
+    }  
+    mem->mem_pb = gtk_progress_bar_new();
+    gtk_box_pack_start(GTK_BOX(mem->box), mem->mem_pb, FALSE, FALSE, 0);
+    gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR(mem->mem_pb), o);
+    gtk_widget_set_size_request(mem->mem_pb, w, h);
+
+    if (mem->show_swap)
+    {
+        mem->swap_pb = gtk_progress_bar_new();
+        gtk_box_pack_start(GTK_BOX(mem->box), mem->swap_pb, FALSE, FALSE, 0);
+        gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR(mem->swap_pb), o);
+        gtk_widget_set_size_request(mem->swap_pb, w, h);
+    }
+
+    gtk_widget_show_all(mem->box);
+    gtk_container_add(GTK_CONTAINER(p->pwid), mem->box);
+    gtk_widget_set_tooltip_markup(mem->plugin.pwid, "XXX");
+    mem_update(mem);
+    mem->timer = g_timeout_add(3000, (GSourceFunc) mem_update, (gpointer)mem);
+    RET(1);
+    
+error:
+    mem_destructor(p);
+    RET(0);
+}
+
 static plugin_class class = {
+    .fname       = NULL,
+    .count       = 0,
     .type        = "mem",
     .name        = "Memory Monitor",
     .version     = "1.0",
---
+++ plugins/mem/mt.h
@@ -0,0 +1,11 @@
+
+/* adds memory types to scan in /proc/meminfo */
+MT_ADD(MemTotal)
+MT_ADD(MemFree)
+MT_ADD(MemShared)
+MT_ADD(Slab)
+MT_ADD(Buffers)
+MT_ADD(Cached)
+
+MT_ADD(SwapTotal)
+MT_ADD(SwapFree)

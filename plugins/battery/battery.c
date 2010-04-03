e434e9a0e3ba6b497dd5d3c04cde3b5275db6250
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2010-04-03 20:04:04 +0000

    battery: added battery icons

--- plugins/battery/battery.c
+++ plugins/battery/battery.c
@@ -16,32 +16,43 @@
 //#define DEBUGPRN
 #include "dbg.h"
 
-static gchar *names[] = {
-    "gpm-battery-000",
-    "gpm-battery-020",
-    "gpm-battery-040",
-    "gpm-battery-060",
-    "gpm-battery-080",
-    "gpm-battery-100",
-};
-
-static gchar *ch_names[] = {
-    "gpm-battery-000-charging",
-    "gpm-battery-020-charging",
-    "gpm-battery-040-charging",
-    "gpm-battery-060-charging",
-    "gpm-battery-080-charging",
-    "gpm-battery-100-charging",
-};
-  
 typedef struct {
     meter_priv meter;
     int timer;
     int level;
     gboolean charging;
+    gchar *sys;
 } battery_priv;
 
 static meter_class *k;
+
+static gchar *batt_working[] = {
+    "battery_0",
+    "battery_1",
+    "battery_2",
+    "battery_3",
+    "battery_4",
+    "battery_5",
+    "battery_6",
+    "battery_7",
+    "battery_8",  
+};
+static gchar **batt_charging = batt_working;
+static gchar *batt_na[] = {
+    "battery_na",
+};
+
+/* Searches in /sys/class/power_supply/ for first directory
+ * representing active battery.
+ * Returns
+ *   Pointer to allocated directory path, must be freed by caller
+ *   NULL - if no battery was found
+ */
+static gchar *
+battery_find_sys(void)
+{
+    return NULL;
+}
 
 static void
 update_battery_status(battery_priv *c, int *level, gboolean *charging)
@@ -64,9 +75,9 @@ battery_get_load(battery_priv *c)
     update_battery_status(c, &level, &charging);
     if (charging != c->charging) {
         if (charging)
-            k->set_icons(&c->meter, G_N_ELEMENTS(ch_names), ch_names);
+            k->set_icons(&c->meter, G_N_ELEMENTS(batt_charging), batt_charging);
         else
-            k->set_icons(&c->meter, G_N_ELEMENTS(names), names);
+            k->set_icons(&c->meter, G_N_ELEMENTS(batt_working), batt_working);
         DBG("seting %s icons\n", charging ? "charging" : "normal");
         c->charging = charging;
     }
@@ -79,10 +90,6 @@ battery_get_load(battery_priv *c)
 }
 
 
-#if 0
-
-#endif
-
 static int
 battery_constructor(plugin_instance *p)
 {
@@ -93,10 +100,18 @@ battery_constructor(plugin_instance *p)
     if (!PLUGIN_CLASS(k)->constructor(p))
         RET(0);
     c = (battery_priv *) p;
-    k->set_icons(&c->meter, G_N_ELEMENTS(names), names);
-    c->timer = g_timeout_add(1000, (GSourceFunc) battery_get_load,
-        (gpointer) c);
-    battery_get_load(c);
+    c->sys = battery_find_sys();
+    if (c->sys) {
+        k->set_icons(&c->meter, G_N_ELEMENTS(batt_working), batt_working);
+        c->timer = g_timeout_add(1000, (GSourceFunc) battery_get_load,
+            (gpointer) c);
+        battery_get_load(c);
+    } else {
+        k->set_icons(&c->meter, G_N_ELEMENTS(batt_na), batt_na);
+        k->set_level(&c->meter, 0.0);
+        gtk_widget_set_tooltip_markup(p->pwid,
+            "Runing on AC\nNo battery found");
+    }
     RET(1);
 }
 
@@ -106,7 +121,9 @@ battery_destructor(plugin_instance *p)
     battery_priv *c = (battery_priv *) p;
 
     ENTER;
-    g_source_remove(c->timer);
+    if (c->timer)
+        g_source_remove(c->timer);
+    g_free(c->sys);
     PLUGIN_CLASS(k)->destructor(p);
     class_put("meter");
     RET();

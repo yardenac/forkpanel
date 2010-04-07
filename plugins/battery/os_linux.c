8e8a56858454bd4130cb20b3ab5acb9c04c887df
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2010-04-07 23:07:48 +0000

    battery: fixed parsing of string values

--- plugins/battery/os_linux.c
+++ plugins/battery/os_linux.c
@@ -1,18 +1,39 @@
 
+#include <string.h>
+#include <ctype.h>
+
 #define LEN 100
 #define PROC_ACPI "/proc/acpi/battery/"
 
+
 static gboolean
-get_token_bool(gchar *buf, gchar *token, gboolean *value)
+get_token_eq(gchar *buf, gchar *token, gchar *value, gboolean *ret)
 {
+    int len;
+    gchar *var;
+
     ENTER;
-    RET(FALSE);
+    len = strlen(token);
+    if (!(var = strstr(buf, token)))
+        RET(FALSE);
+    for (var = var + len; isspace(*var); var++) ;
+    *ret = !strncmp(var, value, strlen(value));
+    RET(TRUE);
 }
 
 static gboolean
 get_token_int(gchar *buf, gchar *token, gint *value)
 {
+    int len;
+    gchar *var;
+
     ENTER;
+    len = strlen(token);
+    if (!(var = strstr(buf, token)))
+        RET(FALSE);
+    for (var = var + len; isspace(*var); var++) ;
+    if (sscanf(var, "%d", value) == 1) 
+        RET(TRUE);
     RET(FALSE);
 }
 
@@ -26,31 +47,34 @@ read_proc(battery_priv *c, GString *path)
     ENTER;
     len = path->len;
     
-    g_string_append(path, "info");
+    g_string_append(path, "/info");
     ret = g_file_get_contents(path->str, &buf, 0, NULL);
+    DBG("reading %s %s\n", path->str, ret ? "ok" : "fail");
     g_string_truncate(path, len);
     if (!ret)
         RET(FALSE);
-    ret = get_token_bool(buf, "present:", &exist)
+    ret = get_token_eq(buf, "present:", "yes", &exist)
         && exist && get_token_int(buf, "design capacity:", &dcap);
     g_free(buf);
     if (!ret)
         RET(FALSE);
-    
-    g_string_append(path, "state");
+
+    g_string_append(path, "/state");
     ret = g_file_get_contents(path->str, &buf, 0, NULL);
+    DBG("reading %s %s\n", path->str, ret ? "ok" : "fail");
     g_string_truncate(path, len);
     if (!ret)
         RET(FALSE);
-    ret = get_token_bool(buf, "present:", &exist)
+    ret = get_token_eq(buf, "present:", "yes", &exist)
         && exist
         && get_token_int(buf, "remaining capacity:", &rcap)
-        && get_token_int(buf, "charging state:", &charging);
+        && get_token_eq(buf, "charging state:", "charging", &charging);
     g_free(buf);
     if (!ret)
         RET(FALSE);
-    DBG("battery=%s\ndesign capacity=%d\nremaining capacity=%d\n",
-        path, dcap, rcap);
+    DBG("battery=%s\ndesign capacity=%d\nremaining capacity=%d\n"
+        "charging=%d\n", 
+        path->str, dcap, rcap, charging);
 
     if (!(dcap >= rcap && dcap > 0 && rcap >= 0))
         RET(FALSE);
@@ -76,13 +100,15 @@ battery_update_os(battery_priv *c)
     path = g_string_sized_new(200);
     g_string_append(path, PROC_ACPI);
     len = path->len;
-    if (!(dir = g_dir_open(path->str, 0, NULL)))
+    if (!(dir = g_dir_open(path->str, 0, NULL))) {
+        DBG("can't open dir %s\n", path->str);
         goto out;
-    
+    }
     while ((file = g_dir_read_name(dir))) {
-        if (!g_file_test(file, G_FILE_TEST_IS_DIR))
-            continue;
         g_string_append(path, file);
+        DBG("testing %s\n", path->str);
+        if (!g_file_test(path->str, G_FILE_TEST_IS_DIR))
+            continue;
         ret = read_proc(c, path);
         g_string_truncate(path, len);
         if (ret)
@@ -92,4 +118,5 @@ battery_update_os(battery_priv *c)
         
 out:
     g_string_free(path, TRUE);
+    RET();
 }

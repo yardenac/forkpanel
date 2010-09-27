ad0a98e8758ddc418618c4b329a9c314c2c9f833
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2010-09-27 21:52:46 +0000

    added script to update PO file with new strings

--- scripts/create_po.sh
+++ scripts/create_po.sh
@@ -17,61 +17,47 @@ Parameters
    version - package version
    destdir - dir to save resulted POT and PO files (default is po/ )
    locale - full locale name, e.g ru_RU.UTF-8
+   translator - name and email of a translator
+   update - update existing PO with new strings
 For example:
    create_po.sh --package=fbpanel --version=6.2 --locale=ru_RU.UTF-8
 EOF
 }
 
-function prn_var ()
-{
-    eval echo \"$1=\$$1\"
-}
 
-function error ()
-{
-    echo "$@"
-    exit 1
-}
+source scripts/funcs.sh
 
-while [ $# -gt 0 ]; do
-    if [ "$1" == "--help" ]; then
-        info
-        help
-        exit 0
-    fi
-    if [ "${1:0:2}" != "--" ]; then
-        error "$1 - not a parameter"
-    fi
-    tmp="${1:2}"
-    var=${tmp%%=*}
-    val=${tmp#*=}
-    if [ "$var" == "$val" ]; then
-        error "$1 - not a parameter"
-    fi
-    eval "$var=\"$val\""
-    shift
-done
-[ -z "$package" ] && error "No package set"
-[ -z "$version" ] && error "No version set"
-[ -z "$locale" ] && error "No locale set"
-[ -z "$destdir" ] && destdir=po
+parse_argv "$@"
+check_var package
+check_var version
+check_var locale
+check_var destdir po
 
 # code
 pot=$destdir/$package.pot
+po=$destdir/$locale.po
+
 list=/tmp/list-$$
 mkdir -p $destdir
 find . -name "*.c" -o -name "*.h" > $list
 
 # Create POT file and set charset to be UTF-8
-xgettext --package-name=$package --package-version=$version \
+2>/dev/null xgettext --package-name=$package --package-version=$version \
     --default-domain=$package --from-code=UTF-8 \
     --force-po -k_ -kc_ -f $list -o - |\
 sed '/^"Content-Type:/ s/CHARSET/UTF-8/' > $pot
-
 echo Created $pot
 
 # create language translation files, POs
-msginit --no-translator --locale=$locale --input=$pot -o $destdir/$locale.po
-
-
-
+if [ -n "$update" ]; then
+    old=/tmp/old-$$
+    cp $po $old
+fi
+tr="${translator:-Automaticaly generated}"
+2>/dev/null msginit --no-translator --locale=$locale --input=$pot -o - |\
+sed "/\"Last-Translator:/ s/Auto.*\\\n/$tr\\\n/" > $po
+if [ -n "$update" ]; then
+    scripts/update_po.sh --old=$old --new=$po
+    rm $old
+fi
+echo Created $po

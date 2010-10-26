cf22012a32570c74989a6f05c7da6ce4b325a3bb
aanatoly <aanatoly@d85e2c34-4b13-0410-ba8a-db183166e696>
2010-10-26 13:09:39 +0000

    applied patch 3039499: FreeBSD cpu support

--- plugins/cpu/cpu.c
+++ plugins/cpu/cpu.c
@@ -1,34 +1,25 @@
+
+
 /*
- * CPU usage plugin to fbpanel
- *
- * Copyright (C) 2004 by Alexandre Pereira da Silva <alexandre.pereira@poli.usp.br>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * Free BSD support
+ * A little bug fixed by Mykola <mykola@2ka.mipt.ru>:)
+ * FreeBSD support added by Andreas Wiese <aw@instandbesetzt.net>
+ * and was extended by Eygene Ryabinkin <rea-fbsd@codelabs.ru>
  */
-/*A little bug fixed by Mykola <mykola@2ka.mipt.ru>:) */
 
-
-
+#include <string.h>
 #include "misc.h"
 #include "../chart/chart.h"
 
 //#define DEBUGPRN
 #include "dbg.h"
+#if defined(__FreeBSD__)
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/sysctl.h>
+#endif
 
-/* cpu.c */
 struct cpu_stat {
     gulong u, n, s, i, w; // user, nice, system, idle, wait
 };
@@ -40,30 +31,75 @@ typedef struct {
     gchar *colors[1];
 } cpu_priv;
 
-
-
 static chart_class *k;
 
 static void cpu_destructor(plugin_instance *p);
 
+
 #if defined __linux__
+static int
+cpu_get_load_real(struct cpu_stat *cpu)
+{
+    FILE *stat;
+
+    memset(cpu, 0, sizeof(struct cpu_stat));
+    stat = fopen("/proc/stat", "r");
+    if(!stat)
+        return -1;
+    fscanf(stat, "cpu %lu %lu %lu %lu %lu", &cpu->u, &cpu->n, &cpu->s,
+        &cpu->i, &cpu->w);
+    fclose(stat);
+    return 0;
+}
+#elif defined __FreeBSD__
+static int
+cpu_get_load_real(struct cpu_stat *s)
+{
+    static int mib[2] = { -1, -1 }, init = 0;
+    size_t j;
+    long ct[CPUSTATES];
+
+    memset(cpu, 0, sizeof(struct cpu_stat));
+    if (init == 0) {
+        j = 2;
+        if (sysctlnametomib("kern.cp_time", mib, &j) != 0)
+            return -1;
+
+        init = 1;
+    }
+
+    j = sizeof(ct);
+    if (sysctl(mib, 2, ct, &j, NULL, 0) != 0)
+        return -1;
+    cpu->u = ct[CP_USER];
+    cpu->n = ct[CP_NICE];
+    cpu->s = ct[CP_SYS];
+    cpu->i = ct[CP_IDLE];
+    cpu->wait = 0;
+
+    return 0;
+}
+#else
+static int
+cpu_get_load_real(struct cpu_stat *s)
+{
+    memset(cpu, 0, sizeof(struct cpu_stat));
+    return 0;
+}
+#endif
+
 static int
 cpu_get_load(cpu_priv *c)
 {
     gfloat a, b;
     struct cpu_stat cpu, cpu_diff;
-    FILE *stat;
     float total;
     gchar buf[40];
 
     ENTER;
     total = 0;
-    stat = fopen("/proc/stat", "r");
-    if(!stat)
+    if (cpu_get_load_real(&cpu))
         goto end;
-    fscanf(stat, "cpu %lu %lu %lu %lu %lu", &cpu.u, &cpu.n, &cpu.s,
-        &cpu.i, &cpu.w);
-    fclose(stat);
 
     cpu_diff.u = cpu.u - c->cpu_prev.u;
     cpu_diff.n = cpu.n - c->cpu_prev.n;
@@ -75,8 +111,8 @@ cpu_get_load(cpu_priv *c)
     a = cpu_diff.u + cpu_diff.n + cpu_diff.s;
     b = a + cpu_diff.i + cpu_diff.w;
     total = b ?  a / b : 1.0;
-    
-end:    
+
+end:
     DBG("total=%f a=%f b=%f\n", total, a, b);
     g_snprintf(buf, sizeof(buf), "<b>Cpu:</b> %d%%", (int)(total * 100));
     gtk_widget_set_tooltip_markup(((plugin_instance *)c)->pwid, buf);
@@ -84,20 +120,12 @@ end:
     RET(TRUE);
 
 }
-#else
-static int
-cpu_get_load(cpu_priv *c)
-{
-    ENTER;
-    RET(0);
-}
-#endif
 
 static int
 cpu_constructor(plugin_instance *p)
 {
     cpu_priv *c;
-    
+
     if (!(k = class_get("chart")))
         RET(0);
     if (!PLUGIN_CLASS(k)->constructor(p))
@@ -105,7 +133,7 @@ cpu_constructor(plugin_instance *p)
     c = (cpu_priv *) p;
     c->colors[0] = "green";
     XCG(p->xc, "Color", &c->colors[0], str);
-    
+
     k->set_rows(&c->chart, 1, c->colors);
     gtk_widget_set_tooltip_markup(((plugin_instance *)c)->pwid, "<b>Cpu</b>");
     cpu_get_load(c);
